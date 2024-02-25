e54c29f79124ea898184231a90a8d25e64e8c8bf
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-02-25 18:45:03 +0100

    debx: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: asus Model: Nexus 7 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_debx.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_debx-user \
+    omni_debx-userdebug \
+    omni_debx-eng
